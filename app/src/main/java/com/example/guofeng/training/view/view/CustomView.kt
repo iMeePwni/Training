64287aed181ed22062597ebf07d65a1c686b855a
guofeng <guofeng@gcall.org>
2017-11-29 10:14:56 +0800

    自定义控件

--- app/src/main/java/com/example/guofeng/training/view/view/CustomView.kt
+++ app/src/main/java/com/example/guofeng/training/view/view/CustomView.kt
@@ -2,30 +2,38 @@ package com.example.guofeng.training.view.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.GestureDetector
 import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
 import android.widget.LinearLayout
+import android.widget.Toast
 import com.example.guofeng.training.R
 import kotlinx.android.synthetic.main.view_custom_view.view.*
+import kotlin.properties.Delegates
 
 /**
  * Created by guofeng on 2017/11/28.
  */
 class CustomView : LinearLayout {
 
+    private var mDetector by Delegates.notNull<GestureDetector>()
 
     constructor(context: Context) : super(context) {
         inflateView()
+        initDetector(context)
     }
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         inflateView()
         initAttrs(context, attrs)
-
+        initDetector(context)
     }
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         inflateView()
         initAttrs(context, attrs)
+        initDetector(context)
     }
 
     private fun initAttrs(context: Context, attrs: AttributeSet) {
@@ -39,9 +47,40 @@ class CustomView : LinearLayout {
         }
     }
 
-
     private fun inflateView() {
         LayoutInflater.from(context).inflate(R.layout.view_custom_view, this)
     }
 
+    private fun initDetector(context: Context) {
+        mDetector = GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onDown(e: MotionEvent?): Boolean = true
+
+            override fun onLongPress(e: MotionEvent?) {
+                super.onLongPress(e)
+                Toast.makeText(context, "YOU PRESS ME TOO LONG", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+                visibility = View.INVISIBLE
+                return super.onFling(e1, e2, velocityX, velocityY)
+            }
+
+        })
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        var result = mDetector.onTouchEvent(event)
+        if (!result) {
+            if (MotionEvent.ACTION_DOWN == event.action) {
+                performClick()
+            }
+            result = super.onTouchEvent(event)
+        }
+        return result
+    }
+
+    override fun performClick(): Boolean {
+        super.performClick()
+        return true
+    }
 }
